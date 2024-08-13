7fc974adcdba3c9019867f0e4c8927869b593d5f
MandaliWala <141039684+MandaliWala@users.noreply.github.com>
2024-08-13 17:44:55 +0530

    Update config.py

--- config.py
+++ config.py
@@ -13,8 +13,7 @@ class Config:
     
     DATABASE_URI = environ.get("DATABASE_URI", "mongodb+srv://fowardbot:fowardbot@cluster0.qjwunlh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
     DATABASE_NAME = environ.get("DATABASE_NAME", "Cluster0")
-    
-    LOG_CHANNEL = int(environ.get('LOG_CHANNEL',"-1002137528664")
+    BOT_OWNER_ID = [int(id) for id in environ.get("BOT_OWNER_ID", '6302921275').split()]
 
 class temp(object): 
     lock = {}
@@ -22,4 +21,3 @@ class temp(object):
     forwardings = 0
     BANNED_USERS = []
     IS_FRWD_CHAT = []
-    
