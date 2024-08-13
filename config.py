f533c3ff79aadf08fc432a43d6acd21557c15467
MandaliWala <141039684+MandaliWala@users.noreply.github.com>
2024-08-13 17:26:18 +0530

    Update config.py

--- config.py
+++ config.py
@@ -15,7 +15,7 @@ class Config:
     DATABASE_NAME = environ.get("DATABASE_NAME", "Cluster0")
     
     LOG_CHANNEL = int(environ.get('LOG_CHANNEL',"-1002137528664")
-    FORCE_SUB_CHANNEL = environ.get("FORCE_SUB_CHANNEL") # FORCE SUB channel link 
+# FORCE SUB channel link 
     FORCE_SUB_ON = environ.get("FORCE_SUB_ON", "False")  # FORCE SUB ON - OFF
 
 
