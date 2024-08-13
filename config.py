69dc7b668b5c0cdb7fb56a9e0e6c01f2403a94e8
MandaliWala <141039684+MandaliWala@users.noreply.github.com>
2024-08-13 18:59:58 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,19 +1,14 @@
 # bot developer @mr_jisshu
-from os import environ 
+fimport os
 
 class Config:
-    
-    API_ID = environ.get("API_ID", "25163484")
-    API_HASH = environ.get("API_HASH", "145bcbc424d1c1ffe04f3e607ea55c9a")
-    BOT_TOKEN = environ.get("BOT_TOKEN", "7062533535:AAGwGXeYVJXvWuoTyAi6ZETTbL26NXjL9zQ") 
-    BOT_OWNER_ID = [int(id) for id in environ.get("BOT_OWNER_ID", '6302921275').split()]
-    BOT_SESSION = environ.get("BOT_SESSION", "bot") 
-
-    PICS = (environ.get('PICS', 'https://graph.org/file/e223aea8aca83e99162bb.jpg'))
-    
-    DATABASE_URI = environ.get("DATABASE_URI", "mongodb+srv://fowardbot:fowardbot@cluster0.qjwunlh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-    DATABASE_NAME = environ.get("DATABASE_NAME", "Cluster0")
-    BOT_OWNER_ID = [int(id) for id in environ.get("BOT_OWNER_ID", '6302921275').split()]
+    API_ID = os.environ.get("API_ID", "25163484")
+    API_HASH = os.environ.get("API_HASH", "145bcbc424d1c1ffe04f3e607ea55c9a")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7062533535:AAGwGXeYVJXvWuoTyAi6ZETTbL26NXjL9zQ") 
+    BOT_SESSION = os.environ.get("BOT_SESSION", "forward-bot") 
+    DB_URL = os.environ.get("DB_URL", "mongodb+srv://fowardbot:fowardbot@cluster0.qjwunlh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+    DB_NAME = os.environ.get("DB_NAME", "Cluster0")
+    OWNER_ID = [int(id) for id in os.environ.get("OWNER_ID", '6302921275').split()]
 
 class temp(object): 
     lock = {}
